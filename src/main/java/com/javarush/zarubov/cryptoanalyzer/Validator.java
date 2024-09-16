28e56a4056fc92025c7c5963e98766ee4ffafba0
Artem Zarubov <art.zarubov@gmail.com>
2024-09-16 20:44:43 +0300

    Чем дальше в лес тем больше делать

--- src/main/java/com/javarush/zarubov/cryptoanalyzer/Validator.java
+++ src/main/java/com/javarush/zarubov/cryptoanalyzer/Validator.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class Validator {
     public static boolean isValidKey(String key, char[] alphabet) {
         try {
-            if (alphabet.length == Integer.parseInt(key) || Integer.parseInt(key) == 0 || key.isBlank()) {
+            if (alphabet.length == Integer.parseInt(key) || Integer.parseInt(key) == 0 || key.isBlank() || Integer.parseInt(key) > 40 || Integer.parseInt(key) < -40) {
                 return false;
             }
         } catch (NumberFormatException exception) {
